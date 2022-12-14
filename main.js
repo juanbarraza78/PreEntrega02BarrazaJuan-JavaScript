1d8e5b3895d1cf9cf011e2ecaeacd228ba2a4f6f
Juan Barraza <juan.barraza7873@hotmail.com>
2022-12-13 22:09:02 -0300

    Entrega 2

---
+++ main.js
@@ -0,0 +1,92 @@
+const dolar = 0.0060;
+const euro = 0.0058;
+const pesos = 5.40;
+
+function convertidorMoneda()
+{
+    let valor = parseInt(document.getElementById("montoCotizador").value);
+    let resultado;
+
+    if(document.getElementById("Euro").checked)
+    {
+        resultado = valor*euro
+        alert(valor.toFixed(2) +" Pesos argentinos son: "+ resultado.toFixed(2)+" Euros");
+    }
+    else if(document.getElementById("Dolar").checked)
+    {
+        resultado = valor*dolar
+        alert(valor.toFixed(2) +" Pesos argentinos son: "+ resultado.toFixed(2)+" Dolares");
+    }
+    else if(document.getElementById("Pesos").checked)
+    {
+        resultado = valor*pesos
+        alert(valor.toFixed(2) +" Pesos argentinos son: "+ resultado.toFixed(2)+" Pesos chilenos");
+    }
+    else
+    {
+        alert("Ingrese los datos correspondientes");
+    }
+
+}
+
+function calcularImporte()
+{
+    let monto = parseInt(document.getElementById("montoImportes").value);
+    let cantidadMeses = parseInt(document.getElementById("meses").value);
+    let porcentajeDeInteres = parseInt(document.getElementById("interes").value);
+    let interes = (porcentajeDeInteres/100)+1;
+    if(interes > 0 && cantidadMeses > 0 && monto > 0)
+    {
+        let total = monto*interes;
+
+        for (let index = 0; index < cantidadMeses-1 ; index++) {
+            total = total*interes; 
+        }
+        alert("El monto " + monto.toFixed(2) + " pasa a ser " + total.toFixed(2) + " luego de estar " + cantidadMeses + " meses a un interes del " + porcentajeDeInteres + " porciento.");
+    }
+    else
+    {
+        alert("Ingrese los datos correspondientes");
+    }
+}
+
+    class Cliente {
+        constructor(nombre, apellido, dni, contrasenia) 
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.dni = dni;
+            this.contrasenia = contrasenia;
+        }
+    }
+
+    const clienteNumero1 = new Cliente("Juan", "Barraza", 42000611, 1234);
+    const clienteNumero2 = new Cliente("a", "Barraza", 42000611, 1);
+    const clienteNumero3 = new Cliente("Ian", "Peres", 42000611, 1234);
+    const clienteNumero4 = new Cliente("Claudia", "Barraza", 42000611, 1234);
+
+    const listaClientes = [clienteNumero1,clienteNumero2,clienteNumero3,clienteNumero4];
+    
+    function ingresarUsuarioContrasenia()
+    {
+        let flag = true;
+        do
+        {
+            let nombre = prompt("ingrese Nombre: ");
+            let contrasenia = parseInt(prompt("ingrese Contraseña: "));
+
+            listaClientes.forEach(cliente => {
+                if(nombre == cliente.nombre && contrasenia == cliente.contrasenia)
+                {
+                    alert("Ingreso correctamente (= ");
+                    flag = false;
+                }
+            });
+        }while(flag);
+    }
+    
+    ingresarUsuarioContrasenia();
+
+    
+
+    
